fd72e8469597f50a8435f6644717ebad86710779
Jay McDoniel <jmcdo29@gmail.com>
2020-07-02 11:49:24 -0700

    creates contactCreator
    Completes challenge for Use Lightning Data Service
    to Work With Data.

--- force-app/main/default/lwc/accountCreator/accountCreator.js
+++ force-app/main/default/lwc/accountCreator/accountCreator.js
@@ -1,4 +1,4 @@
-import { LightningElement } from "lwc";
+import { api, LightningElement } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import ACCOUNT_OBJECT from "@salesforce/schema/Account";
 import NAME_FIELD from "@salesforce/schema/Account.Name";
@@ -6,7 +6,7 @@ import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 
 export default class AccountCreator extends LightningElement {
-  objectApiName = ACCOUNT_OBJECT;
+  @api objectApiName = ACCOUNT_OBJECT;
   fields = [NAME_FIELD, REVENUE_FIELD, INDUSTRY_FIELD];
 
   handleSuccess(event) {
